1d3776a7c40f405ba53d1113aaee0bfaee4c71ee
Jackson Tian <shyvo1987@gmail.com>
2012-10-16 01:44:04 +0800

    Fix test cases

--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@ describe("Doxmate", function () {
     var section = fs.readFileSync(path.join(__dirname, "../README.md"), "utf8");
     var titles = doxmate.getIndexs(section);
     var except = [
-      { level: 1, title: 'Doxmate 不再为文档而发愁'},
+      { level: 1, title: 'Doxmate 不再为文档而发愁 [![Build Status](https://secure.travis-ci.org/JacksonTian/doxmate.png)](http://travis-ci.org/JacksonTian/doxmate)'},
       { level: 2, title: '来源' },
       { level: 2, title: 'Installation' },
       { level: 2, title: 'Usage' },
