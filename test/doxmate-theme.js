9d0e6a7888d10c609809be140adefbbfeadefee6
youxiachai <youxiachai@gmail.com>
2013-08-30 02:11:19 +0800

    update: support custom theme options

---
+++ test/doxmate-theme.js
@@ -0,0 +1,29 @@
+/**
+ * @author: youxiachai
+ * @Date: 13-8-28
+ * @version: 1.0
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+var exec = require('child_process').exec;
+var should = require('should');
+describe('doxmate build', function () {
+  it('should show default themes', function (done) {
+    exec('node ' + __dirname + '/../bin/doxmate theme list', function (err, stdout) {
+      should.not.exist(err);
+      stdout.should.include('default');
+      stdout.should.include('pomelo');
+      stdout.should.include('wordpress');
+      done();
+    });
+  });
+
+  it('should create themes options', function (done) {
+    exec('cd ' + __dirname + '/example' + ' && node ' + __dirname + '/../bin/doxmate theme options', function (err, stdout) {
+      should.not.exist(err);
+      should.exist(stdout);
+      done();
+    });
+  });
+});
